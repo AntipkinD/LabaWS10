586a6f5afbbfd6b98b07c48d5a92c1554607929b
mushroomvoodoo <normeeboy@gmail.com>
2024-05-11 11:37:01 +0300

    ...

--- LabaWS10/Program.cs
+++ LabaWS10/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using LabaWS10;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        SomeClass someobject1 = new("someobject1");
+        SomeClass someobject = new("someobject");
+        someobject.SomeAnotherMethod(someobject.someTitle);
+        someobject1.SomeAnotherMethod(someobject1.someTitle);
+        AnotherClass anotherobject = new("anotherobject");
+        someobject1.SomeAnotherMethod(someobject1.someTitle);
+        someobject.SomeAnotherMethod(someobject.someTitle);
+        someobject.SomeAnotherMethod(someobject.someTitle);
     }
 }
---
+++ LabaWS10/SomeClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWS10
+{
+
+    internal class SomeClass
+    {
+        internal string someTitle { get; set; }
+        internal delegate void SomeDelegate(string text);
+        internal static event SomeDelegate CallR;
+        public SomeClass(string title)
+        {
+            someTitle = title;
+            //SomeAnotherMethod(someTitle);
+        }
+
+        internal void SomeMethod(string title)
+        {
+            title = someTitle;
+            Console.WriteLine(title);
+        }
+        internal void SomeAnotherMethod(string title)
+        {
+            CallR = SomeMethod;
+            SomeDelegate delegati = CallR;
+            delegati?.Invoke(title);
+        }
+    }
+    internal class AnotherClass
+    {
+        internal string anotherTitle { get; set; }
+        public AnotherClass(string title)
+        {
+            anotherTitle = title;
+            SomeClass.CallR += AnotherMethod;
+        }
+        internal void AnotherMethod(string title)
+        {
+            title = anotherTitle;
+            Console.WriteLine(title);
+        }
+    }
+}
